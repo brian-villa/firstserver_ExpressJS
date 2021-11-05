9e9ac4e68868330c0f4c26fd3ad48c567c9be841
Brian Villanova <brian.rj99@yahoo.com.br>
2021-11-05 18:26:25 +0000

    adding publics and statics folder_archieves

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express")
+const path = require("path") //biblioteca do NODE para manipular pastas e caminhos de pastas
 const app = express()
 
-//ROTAS
+//MVC - MODEL(BANCO DE DADOS) VIEW(ARQUIVOS ESTATICOS) CONTROLLER(GERENCIAMENTO DE DADOS)
 
+
+
+//DEFININDO ARQUIVOS ESTÁTICOS
+const staticFolder = path.join(__dirname, 'views') // Definido o caminho da pasta views
+const expressStatic = express.static(staticFolder) // Definido a pasta de arquivos estáticos e retorna o objeto para a const 
+app.use(expressStatic) // e a const é passada para a aplicação
+
+//app.use(express.static(path.join(__dirname, 'views'))) - maneira mais comum de se definir arquivos estaticos e públicos
+
+//DEFININDO ARQUIVOS PÚBLICOS
+const publicFolder = path.join(__dirname, 'public') // Definido o caminho da pasta views
+const expressPublic = express.static(publicFolder) // Definido a pasta de arquivos estáticos e retorna o objeto para a const 
+app.use(expressPublic) // e a const é passada para a aplicação
+
+
+//ROTAS
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.render('views/index')
 }) // end point
+
 
 app.get('/sobre', (req, res) => {
     res.send("Sobre")
@@ -17,7 +35,7 @@ app.use((req, res) => { //middleware
 })
 
 
-
+//EXECUTANDO O SERVIDOR
 
 const gate = process.env.PORT || 8080 // o servidor contratado determina a porta que quer rodar a app e nós usamos essa CONST para permitir a alteração 
 app.listen(gate, () => console.log(`Server is listening on gate ${gate}`))
